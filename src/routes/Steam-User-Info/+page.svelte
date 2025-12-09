a131e25ae8e0d54f40d4732e21c6bc90367b0fe7
timooooo414 <timo.hoeglinger@tfs-haslach.at>
2025-12-09 12:22:19 +0100

    style bootstrap 1

--- src/routes/Steam-User-Info/+page.svelte
+++ src/routes/Steam-User-Info/+page.svelte
@@ -23,231 +23,152 @@
 Der Ihanlt in den ifs kann geändert werden
 -->
 
-{#if !form?.success&&!form?.error}
-	<p>Enter your SteamID in the input field</p>
+{#if !form?.success && !form?.error}
+  <p class="text-light">Enter your SteamID in the input field</p>
 {/if}
 
 {#if form?.error}
-<p>Invalide Inputs (┬┬﹏┬┬)</p>
+  <p class="text-danger fs-4">Invalid Inputs (┬┬﹏┬┬)</p>
 {/if}
 
 {#if form?.success}
-	<div id="carouselExample" class="carousel slide">
-	<div class="carousel-inner">
-		<!---->
-		<div class="carousel-item active">
-			<div class="card mb-3 bg-secondary"
-			style="
-			max-width: 1620px;
-			max-height: 500px;
-			height: 500px;
-			">
-			<div class="row g-0">
-				<div class="col-md-4">
-				<img src="{form?.User1DataJson?.stats.response.players[0].avatarmedium}" alt="Profilbild">
-				</div>
-				<div class="col-md-8">
-				<div class="card-body">
-					<h5 class="card-title">Steam User Info</h5>
-					<a href={form?.User1DataJson?.stats.response.players[0].profileurl} target="_blank"><p class="card-text fs-5">{form?.User1DataJson?.stats.response.players[0].steamid}</p></a>
-					<p class="card-text fs-5">{form?.User1DataJson?.stats.response.players[0].personaname}</p>
-					<p class="card-text"><small class="text-body-secondary">{form?.User1DataJson?.stats.response.players[0].realname}</small></p>
-					<p class="card-text fs-5">Location: {form?.User1DataJson?.stats.response.players[0].loccountrycode}</p>
-					<p class="card-text fs-5">Level: {form?.User1DataJson?.level.response.player_level}</p>
-					<p class="card-text fs-5">Badges: {form?.User1DataJson?.badgeslength}</p>
-					{#if form?.success&&form?.User1DataJson?.grouplist.response.success}
-						<p class="card-text fs-5">Groups: {form?.User1DataJson?.grouplist.response.groups.length}</p>
-					{/if}
+  <div id="carouselExample" class="carousel slide">
+    <div class="carousel-inner">
+      <!-- User Info Card -->
+      <div class="carousel-item active">
+        <div class="card mb-3 bg-dark text-white" style="max-width: 1620px; max-height: 500px; height: 500px;">
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img src="{form?.User1DataJson?.stats.response.players[0].avatarmedium}" class="img-fluid rounded-start" alt="Profile Picture">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title text-info">Steam User Info</h5>
+                <a href="{form?.User1DataJson?.stats.response.players[0].profileurl}" target="_blank">
+                  <p class="card-text fs-5">{form?.User1DataJson?.stats.response.players[0].steamid}</p>
+                </a>
+                <p class="card-text fs-5">{form?.User1DataJson?.stats.response.players[0].personaname}</p>
+                <p class="card-text text-muted">{form?.User1DataJson?.stats.response.players[0].realname}</p>
+                <p class="card-text fs-5">Location: {form?.User1DataJson?.stats.response.players[0].loccountrycode}</p>
+                <p class="card-text fs-5">Level: {form?.User1DataJson?.level.response.player_level}</p>
+                <p class="card-text fs-5">Badges: {form?.User1DataJson?.badgeslength}</p>
+                {#if form?.success && form?.User1DataJson?.grouplist.response.success}
+                  <p class="card-text fs-5">Groups: {form?.User1DataJson?.grouplist.response.groups.length}</p>
+                {/if}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
-				</div>
-				</div>
-			</div>
-			</div>	
-		</div>
-		<!---->
-		<div class="carousel-item">
-			<div class="card mb-3 bg-secondary" 
-			style="
-			max-width: 1620px;
-			max-height: 500px;
-			height: 500px;
-			overflow-y: scroll
-			">
-			<div class="row g-0">
-				<div class="col-md-8">
-				<div class="card-body">		
-					<div class="row">
-						<div class="row">
-						<div class="col-4">
-						</div>
-							<div class="col-8">
-								<div data-bs-spy="scroll" data-bs-target="#simple-list-example" data-bs-offset="0" data-bs-smooth-scroll="true" class="scrollspy-example">
+      <!-- Friends List -->
+      <div class="carousel-item">
+        <div class="card mb-3 bg-dark text-white" style="max-width: 1620px; max-height: 500px; height: 500px; overflow-y: scroll;">
+          <div class="card-body">
+            <h5 class="card-title">Friends List</h5>
+            <div class="list-group">
+              {#each form?.User1DataJson?.friendsdata as friend (friend.response.players[0].steamid)}
+                <a href="{friend.response.players[0].profileurl}" class="list-group-item list-group-item-action bg-dark text-light">
+                  <div class="d-flex justify-content-between align-items-center">
+                    <img src="{friend.response.players[0].avatarmedium}" alt="Profile Picture" class="img-fluid rounded-circle" style="width: 40px; height: 40px;">
+                    <div class="ms-3">
+                      <p class="m-0 fs-5">{friend.response.players[0].personaname}</p>
+                      <small>{friend.response.players[0].realname}</small>
+                    </div>
+                  </div>
+                </a>
+              {/each}
+            </div>
+          </div>
+        </div>
+      </div>
 
-										{#each form?.User1DataJson?.friendsdata as friend (friend.response.players[0].steamid)}
-											<h4 id="simple-list-item-{form?.User1DataJson?.friendsdata.indexOf(friend)}">
-												<p>Name: {friend.response.players[0].personaname}</p>
-											</h4>
-											<p>
-												Steamid: {friend.response.players[0].steamid} | 
-												Name: {friend.response.players[0].realname} , {friend.response.players[0].personaname} | 
-												<a href={friend.response.players[0].profileurl} target="_blank">
-													<img src={friend.response.players[0].avatarmedium} alt="Profilbild">
-												</a>
-											</p>
-										{/each}
-								</div>
-							</div>
-						</div>
-				</div>
-				</div>
-			</div>
-			</div>
-		</div>
-		</div>
-		<!---->
-		<div class="carousel-item">
-			<div class="card mb-3 bg-secondary" 
-			style="
-			max-width: 1620px;
-			max-height: 500px;
-			height: 500px;
-			overflow-y: scroll
-			">
-			<div class="row g-0">
-				<div class="col-md-8">
-				<div class="card-body">		
-					<div class="row">
-						<div class="row">
-						<div class="col-4">
-						</div>
-							<div class="col-8">
-								<div data-bs-spy="scroll" data-bs-target="#simple-list-example" data-bs-offset="0" data-bs-smooth-scroll="true" class="scrollspy-example">
-									<p class="card-text">Games: {form?.User1DataJson?.ownedgames.response.game_count}</p>	
-										{#each form?.User1DataJson?.ownedgames.response.games as game}
-											<h4 id="simple-list-item-{form?.User1DataJson?.friendsdata.indexOf(game)}">
-												<p>Name: {game.name}</p>
-											</h4>
-											<p>
-											</p>
-										{/each}
-								</div>
-							</div>
-						</div>
-				</div>
-				</div>
-			</div>
-			</div>
-		</div>
-		</div>
-		<!---->
-		<div class="carousel-item">
-			<div class="card mb-3 bg-secondary" 
-			style="
-			max-width: 1620px;
-			max-height: 500px;
-			height: 500px;
-			">
-			<div class="row g-0">
-				<div class="col-md-8">
-					<div class="card-body">		
-						<div class="row">
-							{#each form?.User1DataJson?.recentplayedgames.response.games as game}
-								<div class="card w-75 mb-3 bg-secondary" 
-										style="
-										
-										">
-									<div class="card-body">
-										<h5 class="card-title">{game.name}</h5>
-										<table>
-											<tbody>
-												<tr>
-													<td>
-														<p class="card-text">Playtime last  2Weeks: </p>
-													</td>
-													<td>
-														<p class="card-text">{game.playtime_2weeks/60}h</p>
-													</td>
-												</tr>
-												<tr>
-													<td>
-														<p class="card-text">Playtime: </p>
-													</td>
-													<td>
-														<p class="card-text">{game.playtime_forever/60}h</p>
-													</td>
-												</tr>
-												<tr>
-													<td>
-														<p class="card-text">Release Date: </p>
-													</td>
-													<td>
-														<p class="card-text">{form?.User1DataJson?.recentplayedgamesarray[form?.User1DataJson?.recentplayedgames.response.games.indexOf(game)][game.appid]?.data?.release_date.date}</p>
-													</td>
-												</tr>
-												<tr>
-													<td>
-														<img src="{form?.User1DataJson?.recentplayedgamesarray[form?.User1DataJson?.recentplayedgames.response.games.indexOf(game)][game.appid]?.data?.capsule_image}" 
-														alt="{form?.User1DataJson?.recentplayedgamesarray[form?.User1DataJson?.recentplayedgames.response.games.indexOf(game)][game.appid]?.data?.name} capsule image">
-													</td>
-												</tr>
-											</tbody>
-										</table>
-									</div>
-								</div>
-							{/each}
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>	
-		</div>
-		<!---->
-				<div class="carousel-item">
-			<div class="card mb-3 bg-secondary" 
-			style="
-			max-width: 1620px;
-			max-height: 500px;
-			height: 500px;
-			overflow-y: scroll
-			">
-			<div class="row g-0">
-				<div class="col-md-8">
-				<div class="card-body">		
-					<div class="row">
-						<div class="row">
-						<div class="col-4">
-						</div>
-							<div class="col-8">
-								<div data-bs-spy="scroll" data-bs-target="#simple-list-example" data-bs-offset="0" data-bs-smooth-scroll="true" class="scrollspy-example">
-										{#each form?.User1DataJson?.wishlist.response.items as game}
-											<h4 id="simple-list-item-{form?.User1DataJson?.wishlist.response.items.indexOf(game)}">
-												<p>Name: {game.appid}</p>
-												<p>{form?.User1DataJson?.wishlistarray[form?.User1DataJson?.wishlist.response.items.indexOf(game)][game.appid]?.data?.type}</p>
-												<p>{form?.User1DataJson?.wishlistarray[form?.User1DataJson?.wishlist.response.items.indexOf(game)][game.appid]?.data?.name}</p>
-												<p>{form?.User1DataJson?.wishlistarray[form?.User1DataJson?.wishlist.response.items.indexOf(game)][game.appid]?.data?.price_overview?.final_formatted}</p>
-												<a href="https://store.steampowered.com/app/{game.appid}" target="_blank">
-													<img src="{form?.User1DataJson?.wishlistarray[form?.User1DataJson?.wishlist.response.items.indexOf(game)][game.appid]?.data?.capsule_image}" 
-													alt="{form?.User1DataJson?.wishlistarray[form?.User1DataJson?.wishlist.response.items.indexOf(game)][game.appid]?.data?.name} capsule image">
-												</a>
-											</h4>
-										{/each}
-								</div>
-							</div>
-						</div>
-				</div>
-				</div>
-			</div>
-			</div>
-		</div>
-		</div>
-	</div>
-	<button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-		<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-		<span class="visually-hidden">Previous</span>
-	</button>
-	<button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-		<span class="carousel-control-next-icon" aria-hidden="true"></span>
-		<span class="visually-hidden">Next</span>
-	</button>
-	</div>
+      <!-- Games List -->
+      <div class="carousel-item">
+        <div class="card mb-3 bg-dark text-white" style="max-width: 1620px; max-height: 500px; height: 500px; overflow-y: scroll;">
+          <div class="card-body">
+            <h5 class="card-title">Games Owned</h5>
+            {#each form?.User1DataJson?.ownedgames.response.games as game}
+              <div class="card mb-3 bg-secondary">
+                <div class="card-body">
+                  <h6 class="card-title">{game.name}</h6>
+                </div>
+              </div>
+            {/each}
+          </div>
+        </div>
+      </div>
+
+      <!-- Recently Played Games -->
+      <div class="carousel-item">
+        <div class="card mb-3 bg-dark text-white" style="max-width: 1620px; max-height: 500px; height: 500px;">
+          <div class="card-body">
+            <h5 class="card-title">Recently Played Games</h5>
+            <div class="row">
+              {#each form?.User1DataJson?.recentplayedgames.response.games as game}
+                <div class="col-md-6 mb-3">
+                  <div class="card bg-secondary">
+                    <div class="card-body">
+                      <h6 class="card-title">{game.name}</h6>
+                      <table class="table table-dark">
+                        <tbody>
+                          <tr>
+                            <td>Playtime last 2 Weeks:</td>
+                            <td>{game.playtime_2weeks / 60}h</td>
+                          </tr>
+                          <tr>
+                            <td>Total Playtime:</td>
+                            <td>{game.playtime_forever / 60}h</td>
+                          </tr>
+                          <tr>
+                            <td>Release Date:</td>
+                            <td>{form?.User1DataJson?.recentplayedgamesarray[form?.User1DataJson?.recentplayedgames.response.games.indexOf(game)][game.appid]?.data?.release_date.date}</td>
+                          </tr>
+                          <tr>
+                            <td>
+                              <img src="{form?.User1DataJson?.recentplayedgamesarray[form?.User1DataJson?.recentplayedgames.response.games.indexOf(game)][game.appid]?.data?.capsule_image}" alt="{game.name} Capsule" class="img-fluid">
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              {/each}
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Wishlist -->
+      <div class="carousel-item">
+        <div class="card mb-3 bg-dark text-white" style="max-width: 1620px; max-height: 500px; height: 500px; overflow-y: scroll;">
+          <div class="card-body">
+            <h5 class="card-title">Wishlist</h5>
+            {#each form?.User1DataJson?.wishlist.response.items as game}
+              <div class="card mb-3 bg-secondary">
+                <div class="card-body">
+                  <h6 class="card-title">{form?.User1DataJson?.wishlistarray[form?.User1DataJson?.wishlist.response.items.indexOf(game)][game.appid]?.data?.name}</h6>
+                  <p>{form?.User1DataJson?.wishlistarray[form?.User1DataJson?.wishlist.response.items.indexOf(game)][game.appid]?.data?.price_overview?.final_formatted}</p>
+                  <a href="https://store.steampowered.com/app/{game.appid}" target="_blank">
+                    <img src="{form?.User1DataJson?.wishlistarray[form?.User1DataJson?.wishlist.response.items.indexOf(game)][game.appid]?.data?.capsule_image}" alt="{form?.User1DataJson?.wishlistarray[form?.User1DataJson?.wishlist.response.items.indexOf(game)][game.appid]?.data?.name} Capsule Image" class="img-fluid">
+                  </a>
+                </div>
+              </div>
+            {/each}
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
 {/if}
