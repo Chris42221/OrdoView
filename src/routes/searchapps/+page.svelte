323d1065ab64e5a696030ce7545f73f520ffc35c
Chris42221 <christian.lucaci@tfs-haslach.at>
2025-12-16 13:48:36 +0100

    Implement search functionality for apps with form handling and API integration

--- src/routes/searchapps/+page.svelte
+++ src/routes/searchapps/+page.svelte
@@ -1,10 +1,6 @@
 <script>
+	export let data;
 </script>
 
-<form method="POST" action="?/searchapps">
-	<label>
-		Password
-		<input id="AppName" name="AppName" type="text">
-		<button type="submit">Search</button>
-	</label>
-</form>
+
+
