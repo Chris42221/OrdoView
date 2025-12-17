3645170f740e5a975289baff02b2985ba8df189e
Chris42221 <christian.lucaci@tfs-haslach.at>
2025-12-17 12:27:33 +0100

    ka

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
   const jsonStats = data.stats;
 </script>
 
-<form method="POST" action="/searchapps">
+<form method="POST" action="?/SearchApps">
 	<label>
 		<p class="da">Games: </p>
 		<input id="AppName" name="AppName" type="text">
@@ -21,9 +21,11 @@ Die jsonStats.users_online Zeigt die Aktuellen Online User als Zahl an zb 33,333
 
 {#if form?.success}
 	{#each form?.AppSearchResults.getappresults as app}
-		<p>{app.appid}</p>
-		<p>{app.name}</p>
-		<img src="{app.icon}" alt="gameicon">
-		<img src="{app.logo}" alt="gamelogo">
+		<form action="?/GetAppData" method="POST">
+			<p>{app.appid}</p>
+			<button type="submit">
+			<img src="{app.logo}" alt="gamelogo">
+		</button>
+		</form>
 	{/each}
 {/if}
