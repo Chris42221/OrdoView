c2499f2377650592abd451889f274219413dc25c
Chrisi421 <123869476+Chrisi421@users.noreply.github.com>
2026-01-04 18:37:14 +0100

    Refactor GetAppData display: remove redundant information and streamline layout

--- src/routes/GetAppData/+page.svelte
+++ src/routes/GetAppData/+page.svelte
@@ -13,29 +13,7 @@ was dann die API anfrage durchführt
 Zeigt die Daten der API an
 -->
 {#if form?.success && form?.appid && typeof form?.appid === 'string'}
-    <h1>{form?.appinformation.GetAppInformation[form?.appid].data.name}</h1>
-    <h3>{form?.appinformation.GetAppInformation[form?.appid].data.steam_appid}</h3>
-    <p>{form?.appinformation.GetAppInformation[form?.appid].data.about_the_game}</p>
-    <p>{form?.appinformation.GetAppInformation[form?.appid].data.pc_requirements.minimum}</p>
-    <p>{form?.appinformation.GetAppInformation[form?.appid].data.pc_requirements.recommended}</p>
-    <p>{form?.appinformation.GetAppInformation[form?.appid].data.release_date.date}</p>
-    <p><img src={form?.appinformation.GetAppInformation[form?.appid].data.capsule_image}
-        alt={form?.appinformation.GetAppInformation[form?.appid].data.capsule_image}></p>
-
-    {#if form?.appinformation.GetAppInformation[form?.appid].data.price_overview}
-        <p>{form?.appinformation.GetAppInformation[form?.appid].data.price_overview.final_formatted}</p>
-    {/if}
-
-    {#each form?.appinformation.GetAppInformation[form?.appid].data.dlc as dlcId, index}
-        <p>DLC ID: {dlcId}</p>
-        <p>DLC Name: {form?.appinformation.GetDLCInformation[index][dlcId].data.name}</p>
-        <p><img src={form?.appinformation.GetDLCInformation[index][dlcId].data.capsule_image}
-             alt={form?.appinformation.GetDLCInformation[index][dlcId].data.capsule_image}></p>
-    {/each}
-
-    
-
-    <div class="container mt-4 nbg-transparent">
+     <div class="container mt-4 nbg-transparent">
         <div class="card">
             <!-- Game Header -->
             <div class="card-body d-flex">
